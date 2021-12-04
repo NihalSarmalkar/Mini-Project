9bef1261fc2c58d70eb230b0a294841cf4469abd
nihal <snilesh746@gmail.com>
2021-12-04 19:52:28 +0530

    front-end 1.0

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict_crop')
+def predict_crop():
+    return render_template('predict_crop_form.html')
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
